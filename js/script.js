1287e35bd05b961ce6523c44554683be86482daf
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-29 20:09:40 +0200

    final modification 1

--- js/script.js
+++ js/script.js
@@ -2,106 +2,47 @@ console.log('JS OK!');
 
 const images = [ '01', '02', '03', '04', '05'];
 
- const container = document.getElementById('container');
+const container = document.getElementById('container');
 
- let img = '';
-
- for( i = 0 ; i < images.length ; i++ ){
-     img = img + `<img src="img/${images[i]}.jpg" alt="img">`;
-     
- }
+for( i = 0 ; i < images.length ; i++ ){
+    const imageclass = i === 0 ? 'd-block' : 'd-none'
+    let img = '';
+    img = img + `<img src="img/${images[i]}.jpg" class='${imageclass}' alt="img">`;
+     container.innerHTML += img;
+     }
  
- container.innerHTML = img;
- console.log(img);
+ 
+ const skinUp =document.querySelector('.skin-up')
+ const downUp = document.querySelector('.skin-down') 
 
+ let activeIndex = 0
 
+ skinUp.addEventListener('click', function(){
+    if (activeIndex < images.length - 1){
+        container.innerHTML = ''
+        activeIndex++
+        for(let i = 0; i < images.length; i++){
+            const imageclass = i === activeIndex ? 'd-block' : 'd-none'
+            let img = '';
+            img = img + `<img src="img/${images[i]}.jpg" class='${imageclass}' alt="img">`;
+            container.innerHTML += img
+        }
+    }
 
+ }
+ )
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-const images = ['01', '02', '03', '04', '05'];
-let activeIndex = 0;
-
-
-const container = document.querySelector('.container');
-
-for (let i = 0; i < images.length; i++) {
-    const image = images[i];
-    const imageClass = i === activeIndex ? 'item active' : 'item'
-
-    container.innerHTML += `<img
-                                class="${imageClass}"
-                                src="img/${image}.jpg" alt="${image}" 
-                            />`
+downUp.addEventListener('click', function () {
+    if (activeIndex > 0) {
+        container.innerHTML = ''
+        activeIndex--
+        for (let i = 0; i < images.length; i++) {
+            const imageclass = i === activeIndex ? 'd-block' : 'd-none'
+            let img = '';
+            img = img + `<img src="img/${images[i]}.jpg" class='${imageclass}' alt="img">`;
+            container.innerHTML += img
+        }
+    }
 
 }
-
-const previousButton = document.getElementById('previous');
-
-const nextButton = document.getElementById('next');
-
-nextButton.addEventListener('click',
-    function () {
-        console.log('mi hai cliccato!');
-
-        if (activeIndex < images.length - 1) {
-            container.innerHTML = '';
-            activeIndex++;
-
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'item active' : 'item'
-
-                container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
-
-            }
-
-        }
-
-    }
-);
-
-
-previousButton.addEventListener('click',
-    function () {
-        console.log('mi hai cliccato!');
-        if (activeIndex > 0) {
-            container.innerHTML = '';
-            activeIndex--;
-
-            for (let i = 0; i < images.length; i++) {
-                const image = images[i];
-                const imageClass = i === activeIndex ? 'item active' : 'item'
-
-                container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
-
-            }
-
-        }
-    }
-);
-*/
+)
