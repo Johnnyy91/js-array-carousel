b6ab065990a4e7eb90f799522fba5fdaa3969f4b
Johnnyy91 <claudiosardilli@gmail.com>
2022-08-29 16:49:20 +0200

    elaboration part 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,98 @@
-console.log('JS-OK')
+console.log('JS OK!');
+
+const images = [ '01', '02', '03', '04', '05'];
+let arrow = 0;
+
+ const container = document.getElementById('container');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+const images = ['01', '02', '03', '04', '05'];
+let activeIndex = 0;
+
+
+const container = document.querySelector('.container');
+
+for (let i = 0; i < images.length; i++) {
+    const image = images[i];
+    const imageClass = i === activeIndex ? 'item active' : 'item'
+
+    container.innerHTML += `<img
+                                class="${imageClass}"
+                                src="img/${image}.jpg" alt="${image}" 
+                            />`
+
+}
+
+const previousButton = document.getElementById('previous');
+
+const nextButton = document.getElementById('next');
+
+nextButton.addEventListener('click',
+    function () {
+        console.log('mi hai cliccato!');
+
+        if (activeIndex < images.length - 1) {
+            container.innerHTML = '';
+            activeIndex++;
+
+            for (let i = 0; i < images.length; i++) {
+                const image = images[i];
+                const imageClass = i === activeIndex ? 'item active' : 'item'
+
+                container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+
+            }
+
+        }
+
+    }
+);
+
+
+previousButton.addEventListener('click',
+    function () {
+        console.log('mi hai cliccato!');
+        if (activeIndex > 0) {
+            container.innerHTML = '';
+            activeIndex--;
+
+            for (let i = 0; i < images.length; i++) {
+                const image = images[i];
+                const imageClass = i === activeIndex ? 'item active' : 'item'
+
+                container.innerHTML += '<img class="' + imageClass + '" src="img/' + image + '.jpg" alt="due" />'
+
+            }
+
+        }
+    }
+);
+*/
